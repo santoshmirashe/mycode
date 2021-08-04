87b211696b3ecd71cfff56c1bb640520daf636f6
santoshmirashe <santoshmirashe.sm@gmail.com>
2021-08-04 09:49:17 +0530

    first condition

--- Incubytes/src/com/me/StringCalculator.java
+++ Incubytes/src/com/me/StringCalculator.java
@@ -2,9 +2,26 @@ package com.me;
 
 public class StringCalculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static final int add(final String numbers) {
+		int result = 0;
+		String[] numbersArray = numbers.split(",");
+		int len = numbersArray.length;
+		if (len > 2) {
+			throw new RuntimeException("Max 2 number are allowed.");
+		} else {
+			checkIfItsANumber(numbersArray);
+		}
+		return result;
 	}
 
+	private static void checkIfItsANumber(String[] numbersArray) {
+		for (String number : numbersArray) {
+			Integer.parseInt(number);
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "1,2";
+		System.out.println(add(str));
+	}
 }
