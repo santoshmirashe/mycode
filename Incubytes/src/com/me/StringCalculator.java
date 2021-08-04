06a61212317ffcc92feca69c41287afc826c1b23
santoshmirashe <santoshmirashe.sm@gmail.com>
2021-08-04 09:58:32 +0530

    4th condition now it will allow n number of numbers instead of only 2

--- Incubytes/src/com/me/StringCalculator.java
+++ Incubytes/src/com/me/StringCalculator.java
@@ -3,19 +3,14 @@ package com.me;
 public class StringCalculator {
 
 	public static final int add(final String numbers) {
-		int result;
 		String[] numbersArray = numbers.split(",");
-		int len = numbersArray.length;
-		if (len > 2) {
-			throw new RuntimeException("Max 2 number are allowed.");
-		} else {
-			result = add(numbers, numbersArray);
-		}
+		int result = add(numbers, numbersArray);
 		return result;
 	}
 
 	private static int add(String numbersString, String[] numbersArray) {
-		//this method will return 0 if the string is empty else it will add the numbers
+		// this method will return 0 if the string is empty else it will add the
+		// numbers
 		int result = 0;
 		if (!numbersString.trim().isEmpty()) {
 			for (String number : numbersArray) {
