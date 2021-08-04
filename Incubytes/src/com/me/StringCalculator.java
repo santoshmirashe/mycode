cf5ce5c7a772d74f9eae3d643d70bb2310122c49
santoshmirashe <santoshmirashe.sm@gmail.com>
2021-08-04 09:56:34 +0530

    second test case

--- Incubytes/src/com/me/StringCalculator.java
+++ Incubytes/src/com/me/StringCalculator.java
@@ -3,23 +3,28 @@ package com.me;
 public class StringCalculator {
 
 	public static final int add(final String numbers) {
-		int result = 0;
+		int result;
 		String[] numbersArray = numbers.split(",");
 		int len = numbersArray.length;
 		if (len > 2) {
 			throw new RuntimeException("Max 2 number are allowed.");
 		} else {
-			// if String contains other than number 
-			//then this method will throw exception and code will break.
-			checkIfItsANumber(numbersArray);
+			result = add(numbers, numbersArray);
 		}
 		return result;
 	}
 
-	private static void checkIfItsANumber(String[] numbersArray) {
-		for (String number : numbersArray) {
-			Integer.parseInt(number);
+	private static int add(String numbersString, String[] numbersArray) {
+		//this method will return 0 if the string is empty else it will add the numbers
+		int result = 0;
+		if (!numbersString.trim().isEmpty()) {
+			for (String number : numbersArray) {
+				if (!number.trim().isEmpty()) {
+					result += Integer.parseInt(number);
+				}
+			}
 		}
+		return result;
 	}
 
 	public static void main(String[] args) {
